eaff817acb350a29dd283bae4720e815831d2206
maxwellHilmo <maxwellhilmo@gmail.com>
2024-11-13 08:37:06 -0700

    added minor changes.

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,15 +1,11 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+
     get inputUsername () {
-        return $('#username');
+        return $('#user-name');
     }
 
     get inputPassword () {
@@ -17,22 +13,15 @@ class LoginPage extends Page {
     }
 
     get btnSubmit () {
-        return $('button[type="submit"]');
+        return $('input[type="submit"]');
     }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
     async login (username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
     }
-
-    /**
-     * overwrite specific options to adapt it to page object
-     */
+    
     open () {
         return super.open('login');
     }
